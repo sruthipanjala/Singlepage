e524f7396fe18d3c849e29d1575287cc34891a0b
Panjala Sruthi <143275010+sruthipanjala@users.noreply.github.com>
2024-08-16 11:32:41 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About This Blog</h1>
+      <p>This is a simple blog application built with React.</p>
+    </div>
+  );
+};
+
+export default About;
